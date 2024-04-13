0842c3ea413811b7ffbcbbf7faf694381a49754d
TanyaChaudhary010704 <punch010704@gmail.com>
2024-04-13 16:16:11 +0530

    Program added to print greeting

---
+++ 01_basic_Programs/printGreeting.java
@@ -0,0 +1,12 @@
+
+// Take name as input and print a greeting message for that particular name.
+import java.util.*;
+
+public class printGreeting {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    String name = in.next();
+    System.out.println("Hello!" + name);
+  }
+
+}
