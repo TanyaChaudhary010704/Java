aeaafeca421d0eb132853cd66ae792b2c3786c00
TanyaChaudhary010704 <punch010704@gmail.com>
2024-04-14 18:47:22 +0530

    Area of Isosceles Triangle

---
+++ 02_basic_Programs/areaOfIsoscelesTriangle.java
@@ -0,0 +1,16 @@
+
+//Area Of Triangle Java Program
+import java.util.*;
+
+public class areaOfIsoscelesTriangle {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    System.out.print("Enter side1: ");
+    Double a = in.nextDouble();
+    System.out.print("Enter siide2: ");
+    Double b = in.nextDouble();
+    Double area =0.5*Math.sqrt(((a*a - b*b) /4) * b);
+    System.out.println(" Area: " + area);
+    in.close();
+  }
+}
