eaf602ea320c97ebe081ab2feea504fabf80b23d
TanyaChaudhary010704 <punch010704@gmail.com>
2024-04-14 18:24:09 +0530

    Modified AOC

--- 02_basic_Programs/areaOfCircle.java
+++ 02_basic_Programs/areaOfCircle.java
@@ -4,8 +4,8 @@ public class areaOfCircle {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     System.out.print("Enter radius: ");
-    float r = in.nextInt();
-    double area = 2 * Math.PI * r;
+    Double r = in.nextDouble();
+    Double area = Math.PI*r * r;
     System.out.println(" Area: " + area);
     in.close();
   }
