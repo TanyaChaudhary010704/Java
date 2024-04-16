3a38c6d8c51609cd15e741ffde5de982ce4a2800
TanyaChaudhary010704 <punch010704@gmail.com>
2024-04-16 21:01:13 +0530

    parallelogram file updated

---
+++ 03_perimeter_Programs/parallelogram.java
@@ -0,0 +1,15 @@
+//Perimeter Of Parellalogram
+import java.util.*;
+
+public class parallelogram {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    System.out.print("Enter side1: ");
+    Double a = in.nextDouble();
+    System.out.print("Enter side2: ");
+    Double b = in.nextDouble();
+    Double perimeter = 2*(a+b);
+    System.out.println(" Perimeter: " + perimeter);
+    in.close();
+  }
+}
