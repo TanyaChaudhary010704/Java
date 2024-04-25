d66dfeaae7640188da9be28a246d98da027bf6b8
TanyaChaudhary010704 <punch010704@gmail.com>
2024-04-25 20:28:38 +0530

    Calculating depreciation of value

---
+++ 05_Intermediate/depreciation.java
@@ -0,0 +1,14 @@
+//Calculate Depreciation of Value
+import java.util.*;
+public class depreciation {
+  public static void main(String[] args)
+  {
+    //v1 = initial value
+    //t = time 
+    //r = rate of depreciation
+  
+  float V1 = 200, R = 10, T = 2;
+  float D = (float)(V1 * Math.pow((1 - R / 100), T));
+    System.out.print(D); 
+} 
+}
