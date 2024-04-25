c64e7127d760361f9794ebbfd2b46c5d3f5fd632
TanyaChaudhary010704 <punch010704@gmail.com>
2024-04-25 20:18:18 +0530

    using power method of java

---
+++ 05_Intermediate/power.java
@@ -0,0 +1,9 @@
+//power in java 
+import java.util.*;
+public class power {
+  public static void main(String[] args) {
+    double a = 30;
+    double b = 2;
+    System.out.println(Math.pow(a, b));
+  }
+}
