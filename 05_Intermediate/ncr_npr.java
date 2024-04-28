0b7f5c0fcbd0347a94c6e485138297c29487a708
TanyaChaudhary010704 <punch010704@gmail.com>
2024-04-28 21:26:11 +0530

    nCr and nPr

---
+++ 05_Intermediate/ncr_npr.java
@@ -0,0 +1,34 @@
+
+//Fing ncr and npr
+import java.util.*;
+
+public class ncr_npr {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    //ncr = n! / (r! * (n-r)!)
+    //npr = n! / (n-r)!
+    System.out.println("Enter the vlaue of n : ");
+    int n = sc.nextInt();
+    System.out.println("Enter the vlaue of r : ");
+    int r = sc.nextInt();
+
+    // Driver code
+    System.out.println("nCr :"+nCr(n, r));
+    System.out.println("nPr :"+nCr(n, r)* fact(r));
+
+  }
+  static long nCr(int n, int r) {
+    return fact(n) / (fact(r) *
+        fact(n - r));
+  }
+
+  // Returns factorial of n
+  static long fact(int n) {
+    if (n == 0 || n == 1)
+      return 1;
+    long res = 1;
+    for (int i = 2; i <= n; i++)
+      res = res * i;
+    return res;
+  }
+}
