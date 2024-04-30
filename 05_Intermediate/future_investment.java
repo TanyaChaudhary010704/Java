82748f71f0d62028d5b241d2359d51c8939d3e82
TanyaChaudhary010704 <punch010704@gmail.com>
2024-04-30 19:26:00 +0530

    future value

---
+++ 05_Intermediate/future_investment.java
@@ -0,0 +1,15 @@
+//Future Investment Value
+import java.util.*;
+public class future_investment {
+  public static void main(String[] args) {
+    //Present value
+    double p = 1000;
+    //number of months
+    int n = 10;
+    //monthly rate interest in %
+    int r = 1;
+    //future value
+    double fv = p * Math.pow(r + 1, n);
+    System.out.println("Future value : "+ fv);
+  }
+}
